12492a7a09557443cdf1b7f399c7dc3b8ee3c838
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-10-30 16:04:24 -0300

    feat: Add multiplication table script

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('input#btn_tabuada')
+
+btn.addEventListener('click',function(){
+  let num = document.querySelector('input#txtn')
+  let tab = document.querySelector('select#seltab')
+
+  if(num.value.length === 0){
+    alert('[ERROR] Digite um número!')
+  }else{
+    let n = Number(num.value)
+    let c = 1
+    tab.textContent = ''
+    while(c <=10){
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
+})
